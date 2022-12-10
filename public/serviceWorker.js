138728eca5d73c38c386abc8f473bb312641cc70
NokTNL <noklamtsz@gmail.com>
2022-12-10 00:49:54 +0000

    Remove useless caches

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,4 +1,4 @@
-const VERSION = '0.1.2'
+const VERSION = '0.1.3'
 const STATIC_CACHE_NAME = `hangry-nextjs-static-v${VERSION}`
 const DYNAMIC_CACHE_NAME = `hangry-nextjs-dynamic-v${VERSION}`
 
@@ -7,7 +7,7 @@ self.addEventListener('install', event => {
 
   event.waitUntil(
     caches.open(STATIC_CACHE_NAME).then(async cache => {
-      await cache.addAll(['/', '/stores', '/cart'])
+      await cache.addAll(['/stores'])
       console.log('All shell assets cached')
     })
   )
@@ -35,28 +35,31 @@ self.addEventListener('fetch', event => {
         console.log('Used cached assets', response)
         return response
       }
-      return dynamicallyCache(event.request)
+      return fetch(event.request)
+      // return dynamicallyCache(event.request)
     })
   )
 })
 
-/** @type {(request: Request) => Response} */
-async function dynamicallyCache(request) {
-  // try {
-  const response = await fetch(request)
-  const dynamicCache = await caches.open(DYNAMIC_CACHE_NAME)
-  if (response.headers.get('Content-Type').match(/^image\//)) {
-    console.log(
-      `Fetched new image assets and stored in ${DYNAMIC_CACHE_NAME}`,
-      response
-    )
-    await dynamicCache.put(request.url, response.clone())
-  }
-  return response
-  // TODO: return fallback page html
-  // } catch (err) {
-  //   if (request.url.match(/\.html$/)) {
-  //     return caches.match('/pages/fallback.html')
-  //   } else throw err
-  // }
-}
+// /** @param {Request} request */
+// async function dynamicallyCache(request) {
+// try {
+// const response = await fetch(request)
+// TODO: disables image caching for now, does not seem to work on installed apps
+// const dynamicCache = await caches.open(DYNAMIC_CACHE_NAME)
+// if (response.headers.get('Content-Type').match(/^image\//)) {
+//   console.log(
+//     `Fetched new image assets and stored in ${DYNAMIC_CACHE_NAME}`,
+//     response
+//   )
+//   await dynamicCache.put(request.url, response.clone())
+// }
+// return response
+// TODO: return fallback page data
+// } catch (err) {
+//   // Don't handle requests from brwoser extensions
+//   if (request.url.startsWith('http')) {
+//     return caches.match('/_offline')
+//   } else throw err
+// }
+// }
